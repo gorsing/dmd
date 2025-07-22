932b3cfb22dc992fb6e3c17c2cf7c15496fc0885
Kuzko Sergii <skuzko@ibagroup.eu>
2025-07-22 23:54:21 +0300

    refactoring

--- compiler/src/dmd/dtemplate.d
+++ compiler/src/dmd/dtemplate.d
@@ -1313,11 +1313,13 @@ MATCH deduceType(RootObject o, Scope* sc, Type tparam, ref TemplateParameters pa
         alias visit = Visitor.visit;
     public:
         MATCH result;
+        TemplateParameters parameters;
 
-        extern (D) this() @safe
-        {
-            result = MATCH.nomatch;
-        }
+    extern (D) this(ref TemplateParameters parameters)
+    {
+        this.parameters = parameters;
+        result = MATCH.nomatch;
+    }
 
         override void visit(Type t)
         {
@@ -1330,7 +1332,7 @@ MATCH deduceType(RootObject o, Scope* sc, Type tparam, ref TemplateParameters pa
             if (tparam.ty == Tident)
             {
                 // Determine which parameter tparam is
-                size_t i = templateParameterLookup(tparam, &parameters);
+                size_t i = templateParameterLookup(tparam, &this.parameters);
                 if (i == IDX_NOTFOUND)
                 {
                     if (!sc)
@@ -2147,7 +2149,7 @@ MATCH deduceType(RootObject o, Scope* sc, Type tparam, ref TemplateParameters pa
         override void visit(Expression e)
         {
             //printf("Expression.deduceType(e = %s)\n", e.toChars());
-            size_t i = templateParameterLookup(tparam, &parameters);
+            size_t i = templateParameterLookup(tparam, &this.parameters);
             if (i == IDX_NOTFOUND || tparam.isTypeIdentifier().idents.length > 0)
             {
                 if (e == emptyArrayElement && tparam.ty == Tarray)
@@ -2517,7 +2519,7 @@ MATCH deduceType(RootObject o, Scope* sc, Type tparam, ref TemplateParameters pa
         }
     }
 
-    scope DeduceType v = new DeduceType();
+    scope DeduceType v = new DeduceType(&parameters);
     if (Type t = isType(o))
         t.accept(v);
     else if (Expression e = isExpression(o))
