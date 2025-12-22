5de70555691df8c7620949f1699a9aac7cc36a8f
Sergii Kuzko <gorsing444@gmail.com>
2025-12-22 15:31:52 +0300

    refactoring

--- compiler/src/dmd/attribsem.d
+++ compiler/src/dmd/attribsem.d
@@ -2,7 +2,26 @@
  * Does semantic analysis for attributes.
  *
  * The term 'attribute' refers to things that can apply to a larger scope than a single declaration.
+ * Among them are:
+ * - Alignment (`align(8)`)
+ * - User defined attributes (`@UDA`)
+ * - Function Attributes (`@safe`)
+ * - Storage classes (`static`, `__gshared`)
+ * - Mixin declarations  (`mixin("int x;")`)
+ * - Conditional compilation (`static if`, `static foreach`)
+ * - Linkage (`extern(C)`)
+ * - Anonymous structs / unions
+ * - Protection (`private`, `public`)
+ * - Deprecated declarations (`@deprecated`)
+ *
+ * Copyright:   Copyright (C) 1999-2025 by The D Language Foundation, All Rights Reserved
+ * Authors:     $(LINK2 https://www.digitalmars.com, Walter Bright)
+ * License:     $(LINK2 https://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+ * Source:      $(LINK2 https://github.com/dlang/dmd/blob/master/compiler/src/dmd/attribsem.d, _attrib.d)
+ * Documentation:  https://dlang.org/phobos/dmd_attribsem.html
+ * Coverage:    https://codecov.io/gh/dlang/dmd/src/master/compiler/src/dmd/attribsem.d
  */
+
 module dmd.attribsem;
 
 import dmd.arraytypes;
