6e4f9817878458a07ed803c601c21dba644793cd
Sergii Kuzko <gorsing444@gmail.com>
2025-12-22 15:29:21 +0300

    refactoring

--- compiler/src/dmd/attribsem.d
+++ compiler/src/dmd/attribsem.d
@@ -2,26 +2,7 @@
  * Does semantic analysis for attributes.
  *
  * The term 'attribute' refers to things that can apply to a larger scope than a single declaration.
- * Among them are:
- * - Alignment (`align(8)`)
- * - User defined attributes (`@UDA`)
- * - Function Attributes (`@safe`)
- * - Storage classes (`static`, `__gshared`)
- * - Mixin declarations  (`mixin("int x;")`)
- * - Conditional compilation (`static if`, `static foreach`)
- * - Linkage (`extern(C)`)
- * - Anonymous structs / unions
- * - Protection (`private`, `public`)
- * - Deprecated declarations (`@deprecated`)
- *
- * Copyright:   Copyright (C) 1999-2025 by The D Language Foundation, All Rights Reserved
- * Authors:     $(LINK2 https://www.digitalmars.com, Walter Bright)
- * License:     $(LINK2 https://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
- * Source:      $(LINK2 https://github.com/dlang/dmd/blob/master/compiler/src/dmd/attribsem.d, _attrib.d)
- * Documentation:  https://dlang.org/phobos/dmd_attribsem.html
- * Coverage:    https://codecov.io/gh/dlang/dmd/src/master/compiler/src/dmd/attribsem.d
  */
-
 module dmd.attribsem;
 
 import dmd.arraytypes;
@@ -33,19 +14,36 @@ import dmd.expressionsem;
 import dmd.location;
 import dmd.root.array; // for each
 
-
+/**
+ * Retrieves the attributes associated with a UserAttributeDeclaration.
+ * * Returns:
+ * A pointer to Expressions containing the attributes, or null if none exist.
+ */
 Expressions* getAttributes(UserAttributeDeclaration a)
 {
+    // Optimization: check if there is any data to process
+    if (!a.userAttribDecl && (!a.atts || !a.atts.length))
+        return null;
+
     if (auto sc = a._scope)
     {
         a._scope = null;
-        arrayExpressionSemantic(a.atts.peekSlice(), sc);
+        if (a.atts) // Safe semantic call
+            arrayExpressionSemantic(a.atts.peekSlice(), sc);
     }
+
     auto exps = new Expressions();
+
+    // Recursively collect attributes from parent blocks
     if (a.userAttribDecl && a.userAttribDecl !is a)
-        exps.push(new TupleExp(Loc.initial, a.userAttribDecl.getAttributes()));
+    {
+        if (auto parentAtts = a.userAttribDecl.getAttributes())
+            exps.push(new TupleExp(Loc.initial, parentAtts));
+    }
+
     if (a.atts && a.atts.length)
         exps.push(new TupleExp(Loc.initial, a.atts));
+
     return exps;
 }
 
@@ -53,13 +51,13 @@ Expressions* getAttributes(UserAttributeDeclaration a)
  * Iterates the UDAs attached to the given symbol.
  *
  * Params:
- *  sym = the symbol to get the UDAs from
- *  sc = scope to use for semantic analysis of UDAs
- *  dg = called once for each UDA
+ * sym = the symbol to get the UDAs from
+ * sc = scope to use for semantic analysis of UDAs
+ * dg = called once for each UDA
  *
  * Returns:
- *  If `dg` returns `!= 0`, stops the iteration and returns that value.
- *  Otherwise, returns 0.
+ * If `dg` returns `!= 0`, stops the iteration and returns that value.
+ * Otherwise, returns 0.
  */
 int foreachUda(Dsymbol sym, Scope* sc, int delegate(Expression) dg)
 {
@@ -67,18 +65,21 @@ int foreachUda(Dsymbol sym, Scope* sc, int delegate(Expression) dg)
         return 0;
 
     auto udas = sym.userAttribDecl.getAttributes();
+    if (!udas) // PROTECTION: prevent null pointer dereference
+        return 0;
+
     arrayExpressionSemantic(udas.peekSlice(), sc, true);
 
     return udas.each!((uda) {
+        if (!uda) return 0;
+
+        // If it's a tuple (group of UDAs), iterate through its elements
         if (auto te = uda.isTupleExp())
         {
-            return te.exps.each!((e) {
-                assert(e);
-                if (auto result = dg(e))
-                    return result;
-                return 0;
-            });
+            return te.exps.each!((e) => dg(e));
         }
-        return 0;
+
+        // Handle single UDA expression
+        return dg(uda);
     });
 }
