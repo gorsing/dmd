9b50c5adb210240bc3a76493bc8b9d70bf9e0a41
Sergii Kuzko <gorsing444@gmail.com>
2026-03-16 19:57:39 +0300

    add unusedParams

--- compiler/test/fail_compilation/lint_unused_params.d
+++ compiler/test/fail_compilation/lint_unused_params.d
@@ -2,15 +2,32 @@
 REQUIRED_ARGS: -w
 TEST_OUTPUT:
 ---
+fail_compilation/lint_unused_params.d(15): Lint: [unusedParams] function parameter `y` is never used
+fail_compilation/lint_unused_params.d(28): Lint: [unusedParams] function parameter `b` is never used
 ---
 */
 
 pragma(lint, unusedParams);
 
-void test(int x, int y)
+void testBasic(int x, int y)
 {
-    auto z = x + 1;
+    cast(void)x;
+}
+
+class Base { void foo(int a) {} }
+class Derived : Base
+{
+    override void foo(int a) {}
+}
+
+class Normal
+{
+    final void bar(int a, int b)
+    {
+        cast(void)a;
+    }
 }
 
 pragma(lint, none);
+
 void ignored(int a) {}
