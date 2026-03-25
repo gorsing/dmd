cfceea03a7a9a19f52775ed3f1b95dd8730a77a2
Sergii Kuzko <gorsing444@gmail.com>
2026-03-25 16:09:58 +0300

    PoC_v2.1

--- compiler/src/dmd/lint/engine.d
+++ compiler/src/dmd/lint/engine.d
@@ -13,7 +13,6 @@ import dmd.func;
 import dmd.id;
 import dmd.statement;
 import dmd.visitor;
-import dmd.visitor.postorder; // Используем безопасный пост-ордер обход
 import dmd.init;
 
 import dmd.errors : warning;
@@ -26,12 +25,79 @@ extern (D) enum LintFlags : uint
     all          = ~0
 }
 
-// Посетитель для поиска использованных переменных внутри тела функции
-private extern(C++) final class UnusedParamVisitor : StoppableVisitor
+private extern(C++) class DeepVisitor : Visitor
+{
+    alias visit = Visitor.visit;
+    bool[void*] visitedSymbols;
+
+    bool visitDsymbol(Dsymbol s)
+    {
+        if (!s) return false;
+        auto ptr = cast(void*)s;
+        if (ptr in visitedSymbols) return false;
+        visitedSymbols[ptr] = true;
+        return true;
+    }
+
+    override void visit(Dsymbol s) { }
+    override void visit(Statement s) { }
+    override void visit(Expression e) { }
+    override void visit(Initializer i) { }
+
+    override void visit(Module m) { if (visitDsymbol(m) && m.members) foreach(s; *m.members) if (s) s.accept(this); }
+    override void visit(AttribDeclaration ad) { if (visitDsymbol(ad) && ad.decl) foreach(s; *ad.decl) if (s) s.accept(this); }
+    override void visit(AggregateDeclaration ad) { if (visitDsymbol(ad) && ad.members) foreach(s; *ad.members) if (s) s.accept(this); }
+    override void visit(TemplateInstance ti) { if (visitDsymbol(ti) && ti.members) foreach(s; *ti.members) if (s) s.accept(this); }
+    override void visit(TemplateDeclaration td) { if (visitDsymbol(td) && td.members) foreach(s; *td.members) if (s) s.accept(this); }
+    override void visit(TemplateMixin tm) { if (visitDsymbol(tm) && tm.members) foreach(s; *tm.members) if (s) s.accept(this); }
+    override void visit(AliasDeclaration ad) { if (visitDsymbol(ad) && ad.aliassym) ad.aliassym.accept(this); }
+    override void visit(VarDeclaration vd) { if (visitDsymbol(vd) && vd._init) vd._init.accept(this); }
+
+    override void visit(ExpStatement s) { if (s.exp) s.exp.accept(this); }
+    override void visit(ReturnStatement s) { if (s.exp) s.exp.accept(this); }
+    override void visit(IfStatement s) { if (s.condition) s.condition.accept(this); if (s.ifbody) s.ifbody.accept(this); if (s.elsebody) s.elsebody.accept(this); }
+    override void visit(WhileStatement s) { if (s.condition) s.condition.accept(this); if (s._body) s._body.accept(this); }
+    override void visit(DoStatement s) { if (s.condition) s.condition.accept(this); if (s._body) s._body.accept(this); }
+    override void visit(ForStatement s) { if (s._init) s._init.accept(this); if (s.condition) s.condition.accept(this); if (s.increment) s.increment.accept(this); if (s._body) s._body.accept(this); }
+    override void visit(ForeachStatement s) { if (s.aggr) s.aggr.accept(this); if (s._body) s._body.accept(this); }
+    override void visit(ForeachRangeStatement s) { if (s.lwr) s.lwr.accept(this); if (s.upr) s.upr.accept(this); if (s._body) s._body.accept(this); }
+    override void visit(SwitchStatement s) { if (s.condition) s.condition.accept(this); if (s._body) s._body.accept(this); }
+    override void visit(CaseStatement s) { if (s.exp) s.exp.accept(this); if (s.statement) s.statement.accept(this); }
+    override void visit(GotoCaseStatement s) { if (s.exp) s.exp.accept(this); }
+    override void visit(ThrowStatement s) { if (s.exp) s.exp.accept(this); }
+    override void visit(SynchronizedStatement s) { if (s.exp) s.exp.accept(this); if (s._body) s._body.accept(this); }
+    override void visit(WithStatement s) { if (s.exp) s.exp.accept(this); if (s._body) s._body.accept(this); }
+    override void visit(CompoundStatement s) { if (s.statements) foreach(stmt; *s.statements) if (stmt) stmt.accept(this); }
+    override void visit(ScopeStatement s) { if (s.statement) s.statement.accept(this); }
+    override void visit(TryCatchStatement s) { if (s._body) s._body.accept(this); if (s.catches) foreach(c; *s.catches) if (c && c.handler) c.handler.accept(this); }
+    override void visit(TryFinallyStatement s) { if (s._body) s._body.accept(this); if (s.finalbody) s.finalbody.accept(this); }
+    override void visit(LabelStatement s) { if (s.statement) s.statement.accept(this); }
+
+    override void visit(UnaExp e) { if (e.e1) e.e1.accept(this); }
+    override void visit(BinExp e) { if (e.e1) e.e1.accept(this); if (e.e2) e.e2.accept(this); }
+    override void visit(CallExp e) { if (e.e1) e.e1.accept(this); if (e.arguments) foreach(arg; *e.arguments) if (arg) arg.accept(this); }
+    override void visit(ArrayExp e) { if (e.e1) e.e1.accept(this); if (e.arguments) foreach(arg; *e.arguments) if (arg) arg.accept(this); }
+    override void visit(SliceExp e) { if (e.e1) e.e1.accept(this); if (e.lwr) e.lwr.accept(this); if (e.upr) e.upr.accept(this); }
+    override void visit(ArrayLiteralExp e) { if (e.elements) foreach(el; *e.elements) if (el) el.accept(this); }
+    override void visit(AssocArrayLiteralExp e) { if (e.keys) foreach(k; *e.keys) if (k) k.accept(this); if (e.values) foreach(v; *e.values) if (v) v.accept(this); }
+    override void visit(StructLiteralExp e) { if (e.elements) foreach(el; *e.elements) if (el) el.accept(this); }
+    override void visit(TupleExp e) { if (e.e0) e.e0.accept(this); if (e.exps) foreach(el; *e.exps) if (el) el.accept(this); }
+    override void visit(CondExp e) { if (e.econd) e.econd.accept(this); if (e.e1) e.e1.accept(this); if (e.e2) e.e2.accept(this); }
+    override void visit(NewExp e) { if (e.thisexp) e.thisexp.accept(this); if (e.arguments) foreach(arg; *e.arguments) if (arg) arg.accept(this); }
+    override void visit(DeclarationExp e) { if (e.declaration) e.declaration.accept(this); }
+    override void visit(FuncExp fe) { if (fe.fd) fe.fd.accept(this); }
+    override void visit(ScopeExp se) { if (se.sds) se.sds.accept(this); }
+    override void visit(DotTemplateInstanceExp dti) { if (dti.ti) dti.ti.accept(this); if (dti.e1) dti.e1.accept(this); }
+
+    override void visit(ExpInitializer ei) { if (ei.exp) ei.exp.accept(this); }
+    override void visit(StructInitializer si) { foreach(iz; si.value) if (iz) iz.accept(this); }
+    override void visit(ArrayInitializer ai) { foreach(iz; ai.value) if (iz) iz.accept(this); }
+}
+
+private extern(C++) final class UnusedParamVisitor : DeepVisitor
 {
     alias visit = typeof(super).visit;
 
-    // Ключ - указатель на декларацию параметра, значение - был ли он использован
     bool[void*] usedParams;
 
     extern(D) this(VarDeclaration[] paramsToTrack)
@@ -40,76 +106,32 @@ private extern(C++) final class UnusedParamVisitor : StoppableVisitor
             usedParams[cast(void*)p] = false;
     }
 
-    // Заглушки, чтобы избежать fallback'а в базовый класс
-    override void visit(Dsymbol s) { }
-    override void visit(Statement s) { }
-    override void visit(Expression e) { }
-    override void visit(Initializer i) { }
-
-    // --- МОСТЫ: Statements -> Expressions ---
-    // (Без этого walkPostorder не спустится внутрь выражений)
-    override void visit(ExpStatement s) { if (s.exp) walkPostorder(s.exp, this); }
-    override void visit(IfStatement s) { if (s.condition) walkPostorder(s.condition, this); }
-    override void visit(ReturnStatement s) { if (s.exp) walkPostorder(s.exp, this); }
-    override void visit(WhileStatement s) { if (s.condition) walkPostorder(s.condition, this); }
-    override void visit(DoStatement s) { if (s.condition) walkPostorder(s.condition, this); }
-    override void visit(ForStatement s) {
-        if (s.condition) walkPostorder(s.condition, this);
-        if (s.increment) walkPostorder(s.increment, this);
-    }
-    override void visit(ForeachStatement s) { if (s.aggr) walkPostorder(s.aggr, this); }
-    override void visit(ForeachRangeStatement s) {
-        if (s.lwr) walkPostorder(s.lwr, this);
-        if (s.upr) walkPostorder(s.upr, this);
-    }
-    override void visit(SwitchStatement s) { if (s.condition) walkPostorder(s.condition, this); }
-    override void visit(CaseStatement s) { if (s.exp) walkPostorder(s.exp, this); }
-    override void visit(GotoCaseStatement s) { if (s.exp) walkPostorder(s.exp, this); }
-    override void visit(ThrowStatement s) { if (s.exp) walkPostorder(s.exp, this); }
-    override void visit(SynchronizedStatement s) { if (s.exp) walkPostorder(s.exp, this); }
-    override void visit(WithStatement s) { if (s.exp) walkPostorder(s.exp, this); }
-
-    // --- МОСТЫ ДЛЯ ИНИЦИАЛИЗАТОРОВ ---
-    override void visit(DeclarationExp de)
-    {
-        if (de.declaration)
-        {
-            if (auto vd = de.declaration.isVarDeclaration())
-            {
-                if (vd._init)
-                {
-                    if (auto ei = vd._init.isExpInitializer())
-                    {
-                        if (ei.exp) walkPostorder(ei.exp, this);
-                    }
-                }
-            }
-        }
-    }
-
-    // --- МОСТ ДЛЯ ЛЯМБД И ДЕЛЕГАТОВ ---
-    override void visit(FuncExp fe)
-    {
-        if (fe.fd && fe.fd.fbody)
-            walkPostorder(fe.fd.fbody, this); // Ныряем внутрь замыканий!
-    }
-
-    // --- САМОЕ ГЛАВНОЕ: ПЕРЕХВАТ ПЕРЕМЕННЫХ ---
     override void visit(VarExp ve)
     {
         if (ve && ve.var)
         {
             auto ptr = cast(void*)ve.var;
             if (auto p = ptr in usedParams)
-                *p = true; // Отмечаем как использованную
+                *p = true;
         }
+        super.visit(ve);
+    }
+
+    override void visit(SymOffExp se)
+    {
+        if (se && se.var)
+        {
+            auto ptr = cast(void*)se.var;
+            if (auto p = ptr in usedParams)
+                *p = true;
+        }
+        super.visit(se);
     }
 }
 
-// Главный обходчик AST для линтера
-extern(C++) final class LintVisitor : Visitor
+extern(C++) final class LintVisitor : DeepVisitor
 {
-    alias visit = Visitor.visit;
+    alias visit = typeof(super).visit;
 
     LintFlags[] flagsStack;
 
@@ -121,39 +143,6 @@ extern(C++) final class LintVisitor : Visitor
     LintFlags currentFlags()
     {
         return flagsStack.length > 0 ? flagsStack[$ - 1] : LintFlags.none;
-    }
-
-    override void visit(Dsymbol s) { }
-    override void visit(Statement s) { }
-    override void visit(Expression e) { }
-    override void visit(Initializer i) { }
-
-    override void visit(Module m)
-    {
-        if (!m || !m.members) return;
-        foreach (s; *m.members)
-            if (s) s.accept(this);
-    }
-
-    override void visit(AttribDeclaration ad)
-    {
-        if (ad && ad.decl)
-            foreach (s; *ad.decl)
-                if (s) s.accept(this);
-    }
-
-    override void visit(AggregateDeclaration ad)
-    {
-        if (!ad || !ad.members) return;
-        foreach (s; *ad.members)
-            if (s) s.accept(this);
-    }
-
-    override void visit(TemplateInstance ti)
-    {
-        if (!ti || !ti.members) return;
-        foreach (s; *ti.members)
-            if (s) s.accept(this);
     }
 
     private bool pushLintFlags(PragmaDeclaration pd)
@@ -170,7 +159,9 @@ extern(C++) final class LintVisitor : Visitor
     {
         LintFlags newFlags = currentFlags();
         if (!args || args.length == 0)
+        {
             newFlags |= LintFlags.all;
+        }
         else
         {
             foreach (arg; *args)
@@ -194,7 +185,7 @@ extern(C++) final class LintVisitor : Visitor
 
     override void visit(PragmaDeclaration pd)
     {
-        if (!pd) return;
+        if (!visitDsymbol(pd)) return;
         bool pushed = pushLintFlags(pd);
 
         if (pd.decl)
@@ -203,25 +194,6 @@ extern(C++) final class LintVisitor : Visitor
 
         if (pushed)
             flagsStack.length--;
-    }
-
-    // --- Линтер для вложенных функций и прагм внутри тел функций ---
-    override void visit(CompoundStatement s)
-    {
-        if (s && s.statements)
-            foreach (stmt; *s.statements)
-                if (stmt) stmt.accept(this);
-    }
-
-    override void visit(ExpStatement s)
-    {
-        if (s && s.exp) s.exp.accept(this);
-    }
-
-    override void visit(DeclarationExp de)
-    {
-        if (de && de.declaration)
-            de.declaration.accept(this);
     }
 
     override void visit(PragmaStatement ps)
@@ -242,8 +214,6 @@ extern(C++) final class LintVisitor : Visitor
             flagsStack.length--;
     }
 
-    // --- Основная логика ---
-
     private void checkConstSpecial(FuncDeclaration fd)
     {
         if (fd.isGenerated() || (fd.storage_class & STC.const_) || (fd.type && fd.type.isConst()))
@@ -259,10 +229,8 @@ extern(C++) final class LintVisitor : Visitor
         warning(fd.loc, "[constSpecial] special method `%s` should be marked as `const`", fd.ident ? fd.ident.toChars() : fd.toChars());
     }
 
-    override void visit(FuncDeclaration fd)
+    private void lintFunc(FuncDeclaration fd)
     {
-        if (!fd) return;
-
         const flags = currentFlags();
 
         if (flags & LintFlags.constSpecial)
@@ -302,7 +270,7 @@ extern(C++) final class LintVisitor : Visitor
             if (paramsToTrack.length > 0 && fd.fbody)
             {
                 scope paramVisitor = new UnusedParamVisitor(paramsToTrack);
-                walkPostorder(fd.fbody, paramVisitor);
+                fd.fbody.accept(paramVisitor);
 
                 foreach (p; paramsToTrack)
                 {
@@ -316,6 +284,24 @@ extern(C++) final class LintVisitor : Visitor
 
         if (fd.fbody)
             fd.fbody.accept(this);
+    }
+
+    override void visit(FuncDeclaration fd)
+    {
+        if (!visitDsymbol(fd)) return;
+        lintFunc(fd);
+    }
+
+    override void visit(FuncLiteralDeclaration fd)
+    {
+        if (!visitDsymbol(fd)) return;
+        lintFunc(fd);
+    }
+
+    override void visit(CtorDeclaration fd)
+    {
+        if (!visitDsymbol(fd)) return;
+        lintFunc(fd);
     }
 }
 
