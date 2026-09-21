bcadb3d5af97338330e6e0aadc1f9a87774d5225
Sergii K <gorsing444@gmail.com>
2026-09-22 00:21:38 +0300

    Poc v3 pragma lint (#93)
    * Refactoring add pragma lint v11 (#81)
    
    * PoC_v2
    
    * PoC_v2.1
    
    * refactoring fix merge conflict
    
    * refactoring fix merge conflict
    
    * refactoring fix merge conflict
    
    * add lint_struct_params
    
    * add lint_struct_params
    
    * fix compiler/src/dmd/frontend.h
    
    * fix windows error func() -> func
    
    * roleback compiler/test/runnable/template9.d
    
    * delete frontend.g fix megre conflict
    
    Signed-off-by: Sergii Kuzko <gorsing444@gmail.com>
    
    * fix megre conflict
    
    Signed-off-by: Sergii Kuzko <gorsing444@gmail.com>
    
    * fix megre conflict & migrate warning to global.errorSink.warning
    
    Signed-off-by: Sergii Kuzko <gorsing444@gmail.com>
    
    * fix megre conflict & migrate warning to global.errorSink.warning
    
    Signed-off-by: Sergii Kuzko <gorsing444@gmail.com>
    
    * fix megre conflict
    
    Signed-off-by: Sergii Kuzko <gorsing444@gmail.com>
    
    ---------
    
    Signed-off-by: Sergii Kuzko <gorsing444@gmail.com>
    Co-authored-by: Kuzko Sergii <skuzko@ibagroup.eu>

--- compiler/src/dmd/main.d
+++ compiler/src/dmd/main.d
@@ -54,6 +54,7 @@ import dmd.id;
 import dmd.identifier;
 import dmd.inline;
 import dmd.link;
+import dmd.lint.engine : runLinter;
 import dmd.location;
 import dmd.mars;
 import dmd.mtype;
@@ -686,6 +687,8 @@ private int tryMain(const(char)[][] argv, out Param params)
     runDeferredSemantic3();
     if (global.errors)
         removeHdrFilesAndFail(params.dihdr.doOutput, modules);
+
+    runLinter(modules[]);
 
     {
     timeTraceBeginEvent(TimeTraceEventType.inlineGeneral);
